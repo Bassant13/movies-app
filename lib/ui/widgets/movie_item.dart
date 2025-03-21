54f65c539cdef1834e4a12f1eac7e9d541699af5
Your Name <you@example.com>
2025-03-21 07:22:48 +0200

    add new widget

--- lib/ui/widgets/movie_item.dart
+++ lib/ui/widgets/movie_item.dart
@@ -2,52 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:movies_app/core/colors.dart';
 
 class MovieItem extends StatelessWidget{
-  String? rate;
-  String? imageUrl;
-  MovieItem({required this.rate,required this.imageUrl});
+   String? rate;
+   String? imageUrl;
+   Function()? onClick;
+   MovieItem({super.key, required this.rate,required this.imageUrl,this.onClick});
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: Colors.orange,
         borderRadius: BorderRadius.circular(16)
       ),
-      child: Stack(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(16),
-            child: Image.network(
-            imageUrl ?? "",
-            fit: BoxFit.fill,
-            width: 189,
-            height: 279,
-                    ),
-          ),
-           Container(
-            decoration:  BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color:  AppColors.lowOpacityBlack
+      child: InkWell(
+        onTap: onClick,
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: Image.network(
+              imageUrl ?? "",
+              fit: BoxFit.fill,
+             // width: 189,
+              height: 279,
+                      ),
             ),
-            width: 65,
-            height: 32,
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.symmetric(horizontal: 5),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                 Text(
-                   rate ?? "",
-                   style: Theme.of(context).textTheme.bodySmall,
-                 ),
-                Icon(
-                  Icons.star_rounded,
-                  size: 22,
-                  color: AppColors.yellow,
-                )
-              ],
-            ),
-          )
-    ]
+             Container(
+              decoration:  BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color:  AppColors.lowOpacityBlack
+              ),
+              width: 65,
+              height: 32,
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                   Text(
+                     rate ?? "",
+                     style: Theme.of(context).textTheme.bodySmall,
+                   ),
+                  const Icon(
+                    Icons.star_rounded,
+                    size: 22,
+                    color: AppColors.yellow,
+                  )
+                ],
+              ),
+            )
+            ]
+        ),
       ),
 
     );
